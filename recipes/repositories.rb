15d964789c3b3bea9d254f1e24ede591ccd1d57a
Barak Cohen <barak@wondermall.com>
2015-04-26 16:48:19 +0300

    repositories fix

--- recipes/repositories.rb
+++ recipes/repositories.rb
@@ -1,1 +1,1 @@
-node.default[:elasticsearch][:repositories] = Chef::DataBagItem.load('elasticsearch', 'repositories')[node.chef_environment] rescue []
+node.default[:elasticsearch][:repositories] = Chef::DataBagItem.load('elasticsearch', 'repositories')[node.chef_environment] rescue {}
